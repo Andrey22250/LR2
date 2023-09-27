d76295f80d9de8f02b1ba56c4f07e1d5a7775a10
Andrey22250 <andrchepras@mail.ru>
2023-09-28 02:04:50 +0700

    Промежуточный сейв

--- lib.h
+++ lib.h
@@ -2,8 +2,12 @@
 #define len_name 35
 #include <malloc.h>
 #include <string.h>
-#include <stdlib.h>
-#include <stdio.h>
+#include <iostream>
+
+void clean()  //Очистка потока
+{
+	while (getchar() != '\n');
+}
 
 struct CPU
 {
@@ -23,7 +27,7 @@ struct GPU
 struct RAM
 {
 	char *type_ddr;
-	int frequency;
+	int frequency, mem;
 };
 
 struct motherboard
@@ -51,7 +55,7 @@ struct Build
 };
 
 //Вспомогательные функции
-char* StatusTypeToString(Status status) {
+char* StatusToString(Status status) {
 	char* strStatus = (char*)calloc(len_name, sizeof(char));
 
 	switch (status)
@@ -113,7 +117,7 @@ GPU* init_gpu(char* name_gpu, int vram, int TDP)
 	}
 }
 
-RAM* init_RAM(char* type_ddr, int frequency)
+RAM* init_RAM(char* type_ddr, int frequency, int mem)
 {
 	if (type_ddr[0] == 'D' && type_ddr[1] == 'D' && type_ddr[2] == 'R' && frequency > 0)
 	{
@@ -121,6 +125,7 @@ RAM* init_RAM(char* type_ddr, int frequency)
 		ram->type_ddr = (char*)calloc(len_name, sizeof(char));
 		strcpy(ram->type_ddr, type_ddr);
 		ram->frequency = frequency;
+		ram->mem = mem;
 	}
 	else
 	{
@@ -164,7 +169,7 @@ PC* init_PC(CPU cpu, GPU gpu, RAM ram, motherboard motherboard, float price)
 	}
 }
 
-Build* build(int number, PC pc, char* client, Status status)
+Build* init_build(int number, PC pc, char* client, Status status)
 {
 	if (number > 0 && strlen(client) != 0 && status >= create && status <= finished)
 	{
@@ -180,4 +185,154 @@ Build* build(int number, PC pc, char* client, Status status)
 		printf("Неверно, выход из программы...");
 		exit(-1);
 	}
+}
+
+//Ввод структур
+
+CPU input_cpu()
+{
+	printf("Ввод параметров процессора\n");
+	char* name_cpu = (char*)calloc(len_name, sizeof(char));
+	int frequency;
+	int cores, treads;
+	printf("Введите название процессора: ");
+	gets_s(name_cpu, len_name);
+	do
+	{
+		printf("Введите частоту процессора в МГЦ: ");
+		scanf("%d", &frequency);
+		clean();
+	} while (frequency<=0 && frequency>=7000);
+	do
+	{
+		printf("Введите кол-во ядер: ");
+		scanf("%d", &cores);
+		clean();
+	} while (cores <= 0 && cores>256);
+	do
+	{
+		printf("Введите кол-во потоков: ");
+		scanf("%d", &treads);
+		clean();
+	} while (treads <= 0 && treads>512);
+	return *init_cpu(name_cpu, frequency, cores, treads);
+}
+
+GPU input_gpu()
+{
+	printf("Ввод параметров видеокарты\n");
+	char* name_gpu = (char*)calloc(len_name, sizeof(char));
+	int vram, TDP;
+	printf("Введите название видеокарты: ");
+	gets_s(name_gpu, len_name);
+	do
+	{
+		printf("Введите объём видеопамяти в МБ: ");
+		scanf("%d", &vram);
+		clean();
+	} while (vram <= 0);
+	do
+	{
+		printf("Введите TDP в ваттах: ");
+		scanf("%d", &TDP);
+		clean();
+	} while (TDP <= 0);
+	return *init_gpu(name_gpu, vram, TDP);
+}
+
+RAM input_ram()
+{
+	printf("Ввод параметров ОЗУ\n");
+	char* type_ddr = (char*)calloc(len_name, sizeof(char));
+	int frequency, mem;
+	printf("Введите тип ОЗУ: ");
+	gets_s(type_ddr, len_name);
+	do
+	{
+		printf("Введите частоту ОЗУ в МГЦ: ");
+		scanf("%d", &frequency);
+		clean();
+	} while (frequency <= 0 && frequency >= 12000);
+	do
+	{
+		printf("Введите объём ОЗУ: ");
+		scanf("%d", &mem);
+		clean();
+	} while (mem <= 0);
+	return *init_RAM(type_ddr, frequency, mem);
+}
+
+motherboard input_motherboard()
+{
+	printf("Ввод параметров материнской платы\n");
+	char* name_motherboard = (char*)calloc(len_name, sizeof(char));
+	char* chipset = (char*)calloc(len_name, sizeof(char));
+	printf("Введите название материнской платы: ");
+	gets_s(name_motherboard, len_name);
+	printf("Введите чипсет: ");
+	gets_s(chipset, len_name);
+	return *init_motherboard(name_motherboard, chipset);
+}
+
+PC input_pc()
+{
+	float price;
+	printf("Ввод информации о компьютере\n");
+	do
+	{
+		printf("Введите цену ПК: ");
+		scanf("%.2f",&price);
+		clean();
+	} while (price <= 0);
+	return *init_PC(input_cpu(), input_gpu(), input_ram(), input_motherboard(), price);
+}
+
+Build input_build()
+{
+	int number;
+	char* client = (char*)calloc(len_name, sizeof(char));
+	printf("Ввод данных заказа\n");
+	do
+	{
+		printf("Введите номер заказа: ");
+		scanf("%d", &number);
+		clean();
+	} while (number <= 0);
+	printf("Введите имя клиента: ");
+	gets_s(client, len_name);
+	Status status;
+	do
+	{
+		printf("Введите статус заказа, где\n0 - Заказ создан\n1 - Заказ в работе\n2 - Заказ выполнен\nВаш выбор: ");
+		scanf("%d", &status);
+		clean();
+	} while (status < 0 && status > 2);
+	return *init_build(number, input_pc(), client, status);
+}
+
+//Вывод структур
+int Output_PC_spec(PC pc)
+{
+	printf("Информация о сборке:\n\n");
+	printf("Процессор: %s, %d МГЦ, %d ядер, %d потоков\n", pc.cpu.name_cpu, pc.cpu.frequency, pc.cpu.cores, pc.cpu.treads);
+	printf("Видеокарта: %s, %d VRAM, %d TDP\n", pc.gpu.name_gpu, pc.gpu.vram, pc.gpu.TDP);
+	printf("ОЗУ: %s, %d частота, %d объём\n", pc.ram.type_ddr, pc.ram.frequency, pc.ram.mem);
+	printf("Материнская плата: %s, %s чипсет\n", pc.motherboard.name_motherboard, pc.motherboard.chipset);
+	printf("Цена сборки %.2lf\n", pc.price);
+	return 1;
+}
+
+int Output_build(Build build)
+{
+	printf("Информация о заказе:\n\n");
+	printf("Номер заказа: %d\n", build.number);
+	printf("Клиент - %s\n", build.client);
+	printf("Статус - %s\n", StatusToString(build.status));
+}
+
+//Дополнительные функции
+void undervolt_gpu(GPU *gpu)
+{
+	int TDP_lowest = gpu->TDP * 0.85;
+	if()
 }
