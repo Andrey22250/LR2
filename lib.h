fac7486379e5184b7ac2bc5b579fa48d675206be
Andrey22250 <andrchepras@mail.ru>
2023-09-27 09:16:43 +0700

    Чуть подправил

--- lib.h
+++ lib.h
@@ -1,5 +1,7 @@
 #pragma once
 #define len_name 35
+#include <malloc.h>
+#include <string.h>
 
 struct CPU
 {
@@ -18,14 +20,19 @@ struct GPU
 
 struct RAM
 {
-	char type_ddr[7];
+	char *type_ddr;
 	int frequency;
 };
 
 struct motherboard
 {
-	char *name_motherboard;
-	char* chipset;
+	char *name_motherboard, *chipset;
+};
+
+struct psu
+{
+	char* name_psu;
+	int power;
 };
 
 struct PC
@@ -34,6 +41,25 @@ struct PC
 	GPU gpu;
 	RAM ram;
 	motherboard motherboard;
-	char* client;
+	psu psu;
+	float price;
 };
 
+typedef enum Status { create, working, finished };
+
+struct build
+{
+	int number;
+	PC pc;
+	char* client;
+	Status status;
+};
+
+//Вспомогательные функции
+char* check_ddr(RAM *type_ddr)
+{
+	char* str = (char*)calloc(5, sizeof(char));
+	strcpy(str, type_ddr);
+	if (str[0] != 'D' && str[1] != 'D' && str[2]!='R')
+		return
+}
